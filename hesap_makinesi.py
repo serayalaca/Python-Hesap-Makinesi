9c532a4ab8b1caef13ea81bf96db2d0fbc846b69
serayalaca <serayseherim@gmail.com>
2026-02-21 19:40:33 +0300

    Basit 4 işlem hesap makinesi

---
+++ hesap_makinesi.py
@@ -0,0 +1,35 @@
+class HesapMakinesi:
+   
+    def toplama(a, b):
+        return a + b
+
+    def cikarma(a, b):
+        return a - b
+
+    def carpma(a, b):
+        return a * b
+
+    def bolme(a, b):
+        if b != 0:
+            return a / b
+        else:
+            return "Hata: Sıfıra bölünemez!"
+
+sayi1 = float(input("Birinci sayıyı girin: "))
+sayi2 = float(input("İkinci sayıyı girin: "))
+islem = input("İşlem seçin (+, -, *, /): ")
+
+
+if islem == "+":
+    sonuc = HesapMakinesi.toplama(sayi1, sayi2)
+elif islem == "-":
+    sonuc = HesapMakinesi.cikarma(sayi1, sayi2)
+elif islem == "*":
+    sonuc = HesapMakinesi.carpma(sayi1, sayi2)
+elif islem == "/":
+    sonuc = HesapMakinesi.bolme(sayi1, sayi2)
+else:
+    sonuc = "Geçersiz işlem!"
+
+print("------------------")
+print("İşlem Sonucu:", sonuc)
